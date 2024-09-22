5de943cbf451058a757ebd67e51d60d075e401e5
tsukimizake <shomasd@gmail.com>
2024-09-22 19:32:06 +0900

    simplify example

--- examples/Main.hs
+++ examples/Main.hs
@@ -11,14 +11,6 @@ data Z = Z {base :: Polygon, centerPoint :: Point}
 
 mkSketchRes ''Z
 
-data Y = Y {ySide :: Polygon}
-
-mkSketchRes ''Y
-
-data X = X {xSide :: Polygon}
-
-mkSketchRes ''X
-
 main :: IO ()
 main =
   do
@@ -32,20 +24,18 @@ main =
             base <- poly [a, b, c, d]
             centerPoint <- rectCenter a b c d
             pure $ Z {..}
-    let resy = sketch do
+    let yside = sketch do
           (a, b, c, d) <- rectSketch (point & x 0 & y 0) (\a_ -> point & relx a_ 4 & rely a_ 4)
-          ySide <- poly [a, b, c, d]
-          pure $ Y {..}
-    let resx = sketch do
+          poly [a, b, c, d]
+    let xside = sketch do
           a <- point & x 0 & y 0
           b <- point & x 4 & y 0
           c <- point & relx b (-0.5) & y 4
           d <- point & relx a 0.5 & y 4
-          xSide <- poly [a, b, c, d]
-          pure $ X {..}
+          poly [a, b, c, d]
     let obj =
           sketchExtrude 0 10 OnZAxis resz.base
-            & with intersection (sketchExtrude 0 10 OnYAxis resy.ySide)
-            & with intersection (sketchExtrude 0 10 OnXAxis resx.xSide)
+            & with intersection (sketchExtrude 0 10 OnYAxis yside)
+            & with intersection (sketchExtrude 0 10 OnXAxis xside)
             & diff (cylinder 10 1 def & translate (expandVector OnZAxis resz.centerPoint))
     render (pure obj) & writeFile "main.scad"
